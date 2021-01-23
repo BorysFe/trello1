e8974ea847b286bf3c77ca348c3190552d9d9d0e
bfesenko <borys.fesenko@tech-stack.io>
2021-01-23 02:36:44 +0200

    Wait Utils

---
+++ src/main/java/com/trello/WaitUtils.java
@@ -0,0 +1,134 @@
+Waut package com.trello;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class WaitUtils {
+
+    private WebDriver driver;
+
+    private final int shortTimeout = 5;
+    private final int middleTimeout = 10;
+    private final int longTimeout = 15;
+
+    public WaitUtils(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void waitVisibilityOfElement(WebElement element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void waitVisibilityOfElementShort(WebElement element) {
+        waitVisibilityOfElement(element, shortTimeout);
+    }
+
+    public void waitVisibilityOfElementLong(WebElement element) {
+        waitVisibilityOfElement(element, longTimeout);
+    }
+
+    public void waitVisibilityOfListElements(List<WebElement> elements, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
+    public void waitVisibilityOfElementShort(List<WebElement> elements) {
+        waitVisibilityOfListElements(elements, shortTimeout);
+    }
+
+    public void waitVisibilityOfElementLong(List<WebElement> elements) {
+        waitVisibilityOfListElements(elements, longTimeout);
+    }
+
+    public void waitVisibilityOfElementBy(By element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(element));
+    }
+
+    public void waitVisibilityOfElementByShort(By element) {
+        waitVisibilityOfElementBy(element, shortTimeout);
+    }
+
+    public void waitVisibilityOfElementByLong(By element) {
+        waitVisibilityOfElementBy(element, longTimeout);
+    }
+
+    public void waitInvisibilityOfElementBy(By element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.invisibilityOfElementLocated(element));
+    }
+
+    public void waitInvisibilityOfElementByShort(By element) {
+        waitInvisibilityOfElementBy(element, shortTimeout);
+    }
+
+    public void waitInvisibilityOfElementByLong(By element) {
+        waitInvisibilityOfElementBy(element, longTimeout);
+    }
+
+    public void waitInvisibilityOfElement(WebElement element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    public void waitInvisibilityOfElementShort(WebElement element) {
+        waitInvisibilityOfElement(element, shortTimeout);
+    }
+
+    public void waitInvisibilityOfElementLong(WebElement element) {
+        waitInvisibilityOfElement(element, longTimeout);
+    }
+
+    public void waitInvisibilityOfListElements(List<WebElement> elements, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.invisibilityOfAllElements(elements));
+    }
+
+    public void waitInvisibilityOfElementsShort(List<WebElement> elements) {
+        waitInvisibilityOfListElements(elements, shortTimeout);
+    }
+
+    public void waitInvisibilityOfElementsLong(List<WebElement> elements) {
+        waitInvisibilityOfListElements(elements, longTimeout);
+    }
+
+    public void waitPresenceOfElementLocated (By element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.presenceOfElementLocated(element));
+    }
+
+    public void waitPresenceOfElementShort(By element) {
+        waitPresenceOfElementLocated(element, shortTimeout);
+    }
+
+    public void waitPresenceOfElementLong(By element) {
+        waitPresenceOfElementLocated(element, longTimeout);
+    }
+
+    public void waitElementToBeClickable(WebElement element, int timeOut) {
+        WebDriverWait webDriverWait = new WebDriverWait(driver, timeOut);
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public void waitElementToBeClickableShort(WebElement element) {
+        waitElementToBeClickable(element, shortTimeout);
+    }
+
+    public void waitElementToBeClickableLong(WebElement element) {
+        waitElementToBeClickable(element, longTimeout);
+    }
+
+    public void implicitlyWait() {
+
+    }
+//    Wait fluentWait = new FluentWait(WebElement reference)
+//            .withTimeout(Duration.ofSeconds(longTimeout))
+//            .pollingEvery(Duration.ofSeconds(shortTimeout))
+//            .ignoring(Exception.class);
+}
